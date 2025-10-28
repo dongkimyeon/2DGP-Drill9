3baf6f9fac3997f1208190dec9376c4a002253b7
dongkimyeon <114465940+dongkimyeon@users.noreply.github.com>
2025-10-28 16:44:14 +0900

    스페이스바 로직 추가 RUN/IDLE

--- boy.py
+++ boy.py
@@ -124,8 +124,8 @@ class Boy:
             self.IDLE,
             {
                 self.SLEEP : {space_down: self.IDLE},
-                self.IDLE : {time_out: self.SLEEP, right_down: self.RUN, left_down: self.RUN, right_up: self.RUN, left_up: self.RUN},
-                self.RUN : {right_up: self.IDLE, left_up: self.IDLE, right_down: self.IDLE, left_down: self.IDLE}
+                self.IDLE : {time_out: self.SLEEP, right_down: self.RUN, left_down: self.RUN, right_up: self.RUN, left_up: self.RUN, space_down: self.IDLE},
+                self.RUN : {right_up: self.IDLE, left_up: self.IDLE, right_down: self.IDLE, left_down: self.IDLE, space_down: self.RUN},
             }
         )
 
